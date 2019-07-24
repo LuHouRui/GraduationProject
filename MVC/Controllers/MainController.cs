44108675d30f5b6aac0d7e82c70fc123c86aa9ba
厚銳 呂 <drought0621@Gmail.com>
2019-07-24 23:37:17 +0800

    新增Main控制器 以及代派團View

---
+++ MVC/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class MainController : Controller
+    {
+        private DataBaseContext db = new DataBaseContext();
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View(db.Scheule.Where(x=> x.guider == null));
+        }
+    }
+}

3603ea0a57475cc7196352cb95e4f1c5b74474c3
厚銳 呂 <drought0621@Gmail.com>
2019-07-27 23:30:24 +0800

    Revert "多個model同一個view"
    This reverts commit fc28c3892ba63248507383e5f28cab86e1820b2a.

--- MVC/Controllers/MainController.cs
+++ MVC/Controllers/MainController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC.Models;
-using System.Dynamic;
+
 namespace MVC.Controllers
 {
     public class MainController : Controller
@@ -13,8 +13,6 @@ namespace MVC.Controllers
         // GET: Main
         public ActionResult Index()
         {
-            /*http://cs60811.pixnet.net/blog/post/345090569-%E5%A4%9A%E5%80%8Bmodel%E5%9C%A8%E5%90%8C%E4%B8%80%E5%80%8Bview%E4%B8%AD%E9%A1%AF%E7%A4%BA%E7%AF%84%E4%BE%8B*/
-            dynamic models = new ExpandoObject();
             return View(db.Scheule.Where(x=> x.guider == null));
         }
     }

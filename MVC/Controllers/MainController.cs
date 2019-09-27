9657956b1d2691be8e84c1b72e2e2a0f30ac1e1b
厚銳 呂 <drought0621@Gmail.com>
2019-09-28 00:47:36 +0800

    完成ExternalEvent拉進FullCalendar之資料寫入 p.s. ExternalEvent預設日期會因為拉進FullCalendar而被覆蓋，即原本被安排的團體行程會因為主管的隨意拖動而更改出團日期

--- MVC/Controllers/MainController.cs
+++ MVC/Controllers/MainController.cs
@@ -16,64 +16,13 @@ namespace MVC.Controllers
         private DataBaseContext db = new DataBaseContext();
         private Repository repository = new Repository();
         // GET: Main
-        public ActionResult Index()
+        public ActionResult Index(string id = null)
         {
             MainViewModel model = new MainViewModel();
             model.Guiders = db.Guider.ToList();
-            model.Scheulable_Event = db.Scheule.Where(x => x.guider == null).ToList();
+            model.Scheulable_Event = db.Scheule.Where(x => (x.guider == null) || (x.guider == "")).ToList();
+            ViewBag.Number = id;
             return View(model);
-        }
-
-        [HttpGet]
-        public JsonResult GetCalendarData()
-        {
-            // Initialization.  
-            JsonResult result = new JsonResult();
-
-            try
-            {
-                // Loading.  
-                //List<PublicHoliday> data = this.LoadDataOfDb();
-                List<Scheule> scheules = db.Scheule.Where(x => x.guider == null).ToList();
-                // Processing.  
-                result = this.Json(scheules, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                // Info  
-                Console.Write(ex);
-            }
-
-            // Return info.  
-            return result;
-        }
-        [HttpPost]
-        public JsonResult UpdateEvent(int id, DateTime t)
-        {
-            var status = false;
-            try
-            {
-                var result = db.Scheule.Find(id);
-                if (result != null)
-                {
-                    result.start = t;
-                    result.end = t;
-                }
-                db.SaveChanges();
-                status = true;
-            }
-            catch (Exception ex)
-            {
-                // Info  
-                Console.Write(ex);
-            }
-            return new JsonResult { Data = new { status = status } };
-        }
-
-        [HttpPost]
-        public Scheule GetScheule(string title){
-
-            return null;
         }
 
         [HttpPost]
@@ -81,7 +30,10 @@ namespace MVC.Controllers
         {
             // Initialization.  
             JsonResult result = new JsonResult();
-
+            if(id == "Index")
+            {
+                id = null;
+            }
             try
             {
                 // Loading.  
@@ -98,6 +50,29 @@ namespace MVC.Controllers
 
             // Return info.  
             return result;
+        }
+        [HttpPost]
+        public JsonResult UpdateEvent(int id, DateTime t,string guider=null)
+        {
+            var status = false;
+            try
+            {
+                var result = db.Scheule.Find(id);
+                if (result != null)
+                {
+                    result.start = t;
+                    result.end = t;
+                    result.guider = guider;
+                }
+                db.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                // Info  
+                Console.Write(ex);
+            }
+            return new JsonResult { Data = new { status = status } };
         }
     }
 }

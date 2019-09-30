8e8e8672e45fce73373869cf7746154066aae48f
厚銳 呂 <drought0621@Gmail.com>
2019-09-30 21:48:13 +0800

    前端更新

--- MVC/Controllers/MainController.cs
+++ MVC/Controllers/MainController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC.Models;
+using Newtonsoft.Json;
 
 namespace MVC.Controllers
 {
@@ -23,6 +24,106 @@ namespace MVC.Controllers
             model.Scheulable_Event = db.Scheule.Where(x => (x.guider == null) || (x.guider == "")).ToList();
             ViewBag.Number = id;
             return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            List<City> cityList = db.City.ToList();
+            List<Village> villageList = new List<Village>();
+            ViewBag.CityList = cityList;
+            ViewBag.VillageList = villageList;
+            return View(new FormUserData());
+        }
+        [HttpPost]
+        public ActionResult Register(FormUserData data)
+        {
+
+            if (string.IsNullOrWhiteSpace(data.password1) || data.password1 != data.password2)
+            {
+                List<City> cityList = db.City.ToList();
+                List<Village> villageList = new List<Village>();
+                if (!string.IsNullOrWhiteSpace(data.city))
+                    villageList = db.Village.Where(x=>x.CityId == data.city).ToList();
+                ViewBag.CityList = cityList;
+                ViewBag.VillageList = villageList;
+                ViewBag.Msg = "密碼輸入錯誤";
+                return View(data);
+            }
+            else
+            {
+                if (AddUserData(data))
+                {
+                    Response.Redirect("~/Main/Login");
+                    return new EmptyResult();
+                }
+                else
+                {
+                    ViewBag.Msg = "註冊失敗...";
+                    return View(data);
+                }
+            }
+        }
+
+        public bool AddUserData(FormUserData data)
+        {
+            try
+            {
+                UserData user = new UserData();
+                user.UID = data.UID;
+                user.account = data.account;
+                user.password = data.password1;
+                user.city = data.city;
+                user.village = data.village;
+                user.address = data.address;
+                db.UserData.Add(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Village(string id = "")
+        {
+            List<Village> list = db.Village.Where(x=> x.CityId == id).ToList();
+            string result = "";
+            if (list == null)
+            {
+                //讀取資料庫錯誤
+                return Json(result);
+            }
+            else
+            {
+                result = JsonConvert.SerializeObject(list);
+                return Json(result);
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection post)
+        {
+            string account = post["account"];
+            string password = post["password"];
+
+            //驗證密碼
+            var result = db.UserData.Where(x => x.account == account && x.password == password);
+            if (result != null)
+            {
+                Response.Redirect("~/Main/Index");
+                return new EmptyResult();
+            }
+            else
+            {
+                ViewBag.Msg = "登入失敗...";
+                return View();
+            }
         }
 
         [HttpPost]
--- MVC/Models/FormUserData.cs
+++ MVC/Models/FormUserData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,17 +11,26 @@ namespace MVC.Models
     {
         [Key]
         [Required]
+        [DisplayName("編號")]
+        public int UID { get; set; }
+        [Required]
+        [DisplayName("帳號")]
         public string account { get; set; }
         [Required]
+        [DisplayName("密碼")]
         [DataType(DataType.Password)]
         public string password1 { get; set; }
         [Required]
+        [DisplayName("確認密碼")]
         [DataType(DataType.Password)]
         public string password2 { get; set; }
         [Required]
+        [DisplayName("縣市")]
         public string city { get; set; }
         [Required]
+        [DisplayName("鄉鎮")]
         public string village { get; set; }
+        [DisplayName("地址")]
         public string address { get; set; }
     }
 }
